e2217385af019a0a6015db432138f72dd913ab69
Giménez Silva Germén Alberto <ggerman@gmail.com>
2026-03-10 16:37:09 -0300

    udpate post text

--- demo/rsn_events_bot/publish.rb
+++ demo/rsn_events_bot/publish.rb
@@ -2,7 +2,8 @@ require_relative "publishers/mastodon"
 require_relative "publishers/bluesky"
 require_relative "publishers/discord"
 
-message = "Ruby events this week 🧵"
+message = "Ruby events this week 🧵\n🤖 Generated automatically by RubyEventsBot using ruby-libgd.
+Updated every 7 days.\n#Ruby #RubyEvents #RubyLanguage #RubyOnRails"
 image   = "demo/rsn_events_bot/rsn_events.png"
 
 publishers = [
