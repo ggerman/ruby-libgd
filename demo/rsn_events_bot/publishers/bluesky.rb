e597428a0a3dac962d048c3a2b916f2e73316cec
Giménez Silva Germén Alberto <ggerman@gmail.com>
2026-03-10 15:27:30 -0300

    fixing bluesky connection

--- demo/rsn_events_bot/publishers/bluesky.rb
+++ demo/rsn_events_bot/publishers/bluesky.rb
@@ -1,25 +1,3 @@
-require "atproto"
-
-module Publishers
-  class Bluesky
-    def post(text, image_path)
-      identifier = ENV["BSKY_USER"],
-      password = ENV["BSKY_PASS"]
-
-      return false unless identifier && password
-
-      client = Atproto::Client.new
-
-      client.login(
-        identifier: identifier,
-        password: password
-      )
-
-      client.create_post(text: text)
-    end
-  end
-end
-
 require "net/http"
 require "json"
 require "time"
